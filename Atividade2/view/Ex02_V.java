c8dcd98f0c7e5987b84944a24ba1ced0307fba6f
Rodrigo Vinicius <159500431+RodrigoVf7@users.noreply.github.com>
2024-02-26 21:35:08 -0300

    Add files via upload

---
+++ Atividade2/view/Ex02_V.java
@@ -0,0 +1,14 @@
+package atividade2_2024_V;
+
+import atividade2_2024_C.Ex02_C;
+
+public class Ex02_V {
+	public static void main (String[] args) {
+		Ex02_C objt = new Ex02_C();
+		
+		int dividendo = 5;
+		int divisor = 3;
+		int resultado = objt.restoDiv(dividendo, divisor);
+		System.out.println(resultado);
+	}
+}
