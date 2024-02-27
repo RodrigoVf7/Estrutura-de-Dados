6988c5f428e4f3c3b06e8964a2b3ef747bf59ab4
Rodrigo Vinicius <159500431+RodrigoVf7@users.noreply.github.com>
2024-02-26 21:00:58 -0300

    Add files via upload

---
+++ Atividade2/view/Ex01_V.java
@@ -0,0 +1,14 @@
+package atividade2_2024_V;
+
+import atividade2_2024_C.Ex01_C;
+
+public class Ex01_V {
+	public static void main (String[] args) {
+		Ex01_C objt = new Ex01_C();
+		
+		int a = 5;
+		int b = 3;
+		int resultado = objt.SomaVezes(a, b);
+		System.out.println(resultado);
+	}
+}
